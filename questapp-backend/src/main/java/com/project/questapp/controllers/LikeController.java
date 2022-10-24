e0759a925eea884a6e206436e5a639d942aba009
emirhanusta <emirhan1usta@gmail.com>
2022-10-24 17:21:59 +0300

    bug fixed

--- questapp-backend/src/main/java/com/project/questapp/controllers/LikeController.java
+++ questapp-backend/src/main/java/com/project/questapp/controllers/LikeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,7 +35,7 @@ public class LikeController {
 		return likeService.getOneLikeById(likeId);
 	}
 	@PostMapping
-	public Like createOneLike(@RequestParam LikeCreateRequest likeRequest) {
+	public Like createOneLike(@RequestBody LikeCreateRequest likeRequest) {
 		return likeService.createOneLike(likeRequest);
 	}
 	@DeleteMapping("/{likeId}")
