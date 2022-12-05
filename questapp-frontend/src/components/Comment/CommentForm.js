2a751857889eaff42c1619dbcd7f1ac0ca2b21a3
emirhanusta <emirhan1usta@gmail.com>
2022-12-05 13:06:59 +0300

    refresh token added

--- questapp-frontend/src/components/Comment/CommentForm.js
+++ questapp-frontend/src/components/Comment/CommentForm.js
@@ -1,10 +1,8 @@
-import React,{useState} from "react";
-import {Link} from "react-router-dom";
+import React, {useState} from "react";
+import {Link, useHistory} from "react-router-dom";
 import { makeStyles } from '@material-ui/core/styles';
-import { CardContent, InputAdornment, OutlinedInput, Avatar} from "@material-ui/core";
-import Button from "@material-ui/core/Button";
-import { PostWithAuth } from "../../services/HttpService";
-
+import { CardContent, InputAdornment, OutlinedInput, Avatar, Button} from "@material-ui/core";
+import { PostWithAuth, RefreshToken } from "../../services/HttpService";
 const useStyles = makeStyles((theme) => ({
     comment : {
         display: "flex",
@@ -23,40 +21,72 @@ const useStyles = makeStyles((theme) => ({
       }
   }));
 
-export default function Comment(props) {
- 
-    const { userId, userName,postId,setCommentRefresh} = props;
+function CommentForm(props) {
+    const {userId, userName, postId, setCommentRefresh} = props;
     const classes = useStyles();
     const [text, setText] = useState("");
 
+    let history = useHistory();
+
+    const logout = () => {
+      localStorage.removeItem("tokenKey")
+      localStorage.removeItem("currentUser")
+      localStorage.removeItem("refreshKey")
+      localStorage.removeItem("userName")
+      history.go(0)
+    }
 
     const saveComment = () => {
-        PostWithAuth("comment", {
-                postId: postId,
-                userId: userId,
-                text: text,
-            })        
-        .then((res)=>res.json())
-        .catch((err)=> console.log("error"))
+        PostWithAuth("/comments",{
+            postId: postId, 
+            userId : userId,
+            text : text,
+          })
+          .then((res) => {
+            if(!res.ok) {
+                RefreshToken()
+                .then((res) => { if(!res.ok) {
+                    logout();
+                } else {
+                   return res.json()
+                }})
+                .then((result) => {
+                    console.log(result)
+
+                    if(result != undefined){
+                        localStorage.setItem("tokenKey",result.accessToken);
+                        saveComment();
+                        setCommentRefresh();
+                    }})
+                .catch((err) => {
+                    console.log(err)
+                })
+            } else 
+            res.json()
+        })
+          .catch((err) => {
+            console.log(err)
+          })
     }
 
     const handleSubmit = () => {
-      saveComment(); 
-      setText("");
-      setCommentRefresh();
+        saveComment();
+        setText("");
+        setCommentRefresh();
     }
+
     const handleChange = (value) => {
         setText(value);
     }
     return(
-
         <CardContent className = {classes.comment}>
+
         <OutlinedInput
         id="outlined-adornment-amount"
         multiline
         inputProps = {{maxLength : 250}}
         fullWidth 
-        onChange={(e) => handleChange(e.target.value)}   
+        onChange = {(i) => handleChange(i.target.value)}   
         startAdornment = {
             <InputAdornment position="start">
                 <Link  className={classes.link} to={{pathname : '/users/' + userId}}>
@@ -76,10 +106,13 @@ export default function Comment(props) {
             >Comment</Button>
             </InputAdornment>
         }
-       value = {text}
+        value = {text}
         style = {{ color : "black",backgroundColor: 'white'}}
         ></OutlinedInput>
         </CardContent>
 
     )
 }
+
+
+export default CommentForm;
